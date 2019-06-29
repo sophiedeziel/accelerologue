dc7c93af2ecc5647f5501af65340f2a38c0b71aa
Sophie Déziel <courrier@sophiedeziel.com>
2019-06-29 19:51:48 -0400

    Y ajouter un serveur HTTP

--- accelerologue.ino
+++ accelerologue.ino
@@ -17,6 +17,19 @@ bool lastState = false;
 long last_toggle;
 int filenumber = 0;
 
+/* Put your SSID & Password */
+const char* ssid = "Flight Controller";  // Enter SSID here
+const char* password = "Fiouuu123";  //Enter Password here
+
+bool cardWorking = false;
+
+/* Put IP Address details */
+IPAddress local_ip(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+WebServer server(80);
+
 void setup() {
   Serial.begin(115200);
   pinMode(MOSI, INPUT_PULLUP);
@@ -66,12 +79,26 @@ void setup() {
     Serial.println("UNKNOWN");
   }
   Serial.println("card initialized.");
+  cardWorking = true;
   do {
     incrementFileName();
   } while (SD.exists(filename));
+
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+
+
+  server.on("/", handle_OnConnect);
+  server.on("/start", handle_start);
+  server.on("/stop", handle_stop);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
 }
 
 void loop() {
+  server.handleClient();
   String dataString = "";
 
   if ((millis() - last_toggle > 200) && digitalRead(buttonPin) == HIGH && lastState == false) {
@@ -137,6 +164,29 @@ void getAccData() {
   GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 }
 
+void handle_OnConnect() {
+  Serial.println("Recording Status Status: OFF");
+  server.send(200, "text/html", SendHTML(recording));
+}
+
+void handle_start() {
+  recording = true;
+  digitalWrite(LEDPin, recording);
+  Serial.println("Recording Status: ON");
+  server.send(200, "text/html", SendHTML(recording));
+}
+
+void handle_stop() {
+  recording = false;
+  digitalWrite(LEDPin, recording);
+  Serial.println("Recording Status: OFF");
+  server.send(200, "text/html", SendHTML(recording));
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
 void toggleRecord() {
   recording = !recording;
   if (recording) {
@@ -146,4 +196,42 @@ void toggleRecord() {
     incrementFileName();
   }
   digitalWrite(LEDPin, recording);
+}
+
+String SendHTML(bool recording) {
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>Record Control</title>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+  ptr += ".button-on {background-color: #3498db;}\n";
+  ptr += ".button-on:active {background-color: #2980b9;}\n";
+  ptr += ".button-off {background-color: #34495e;}\n";
+  ptr += ".button-off:active {background-color: #2c3e50;}\n";
+  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<h1>Flight Record Control</h1>\n";
+  ptr += "<h3>Using Access Point(AP) Mode</h3>\n";
+
+  if (cardWorking) {
+    ptr += "<h3>SD card loaded</h3>\n";
+  } else {
+    ptr += "<h3 style='color: #dd0000'>SD card not loaded</h3>\n";
+  }
+
+  if (recording)
+  {
+    ptr += "<p>Recording Status: ON</p><a class=\"button button-off\" href=\"/stop\">OFF</a>\n";
+  }
+  else
+  {
+    ptr += "<p>Recording Status: OFF</p><a class=\"button button-on\" href=\"/start\">ON</a>\n";
+  }
+
+  ptr += "</body>\n";
+  ptr += "</html>\n";
+  return ptr;
 }
